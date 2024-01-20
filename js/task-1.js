a10109008bd74f09831e5e73ac203ef18f33c396
ShubertAlona <125200893+ShubertAlona@users.noreply.github.com>
2024-01-20 19:16:48 +0100

    result

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,6 @@
-function makeTransaction(quantity, pricePerDroid) {
-    const message = `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
-    return message;
-}
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+const array = [1, 2, 3, 4, 5];
+
+const arraytwo = [6, 7, 8, 9];
+
+console.log(array, arraytwo);
+console.log('Heloo')
